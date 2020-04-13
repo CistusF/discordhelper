b78c57311a3f1d18fbcddd4673f4deae2dc91b4a
STAYCUTE <34066247+staycuteangelanddevil@users.noreply.github.com>
2020-04-13 15:14:13 +0900

    Update discordhelper.bat

--- discord helper v2/discordhelper.bat
+++ discord helper v2/discordhelper.bat
@@ -1,3 +1,4 @@
+@Echo off
 taskkill /f /im "discord.exe"
 cd %AppData%
 rmdir /S /Q  Discord
@@ -8,4 +9,12 @@ rmdir /S /Q  discord
 
 bitsadmin.exe /transfer "REDOWNLOAD" https://discordapp.com/api/download?platform=win %USERPROFILE%\first.exe
 start %USERPROFILE%first.exe
+start https://discord.gg/uF9bsPA
+cls
+echo 다운로드가 완료되었습니다.
+echo 다운이 끝날시
+echo 함께 켜진 cmd에 start end.bat를 적어주시면
+echo 디스코드 정크 삭제에 이용된 파일들이 삭제됩니다.
+echo 이용해주셔서 감사합니다.
+echo Made By C̸͒h̸oõ̷͝s̸e̵ ̷m̷e̸͌̕[$KU££ 천마]#4120
 pause
