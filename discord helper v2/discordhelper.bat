10654e293036b218b1b1fa1d5fe26b079202ec49
STAYCUTE <34066247+staycuteangelanddevil@users.noreply.github.com>
2020-04-13 15:18:34 +0900

    Update discordhelper.bat

--- discord helper v2/discordhelper.bat
+++ discord helper v2/discordhelper.bat
@@ -6,10 +6,10 @@ rmdir /S /Q  discord
 cd %LocalAppData%
 rmdir /S /Q  Discord
 rmdir /S /Q  discord
-
 bitsadmin.exe /transfer "REDOWNLOAD" https://discordapp.com/api/download?platform=win %USERPROFILE%\first.exe
 start %USERPROFILE%first.exe
 start https://discord.gg/uF9bsPA
+echo del /q "first.exe" del /q "end.bat" > end.bat
 cls
 echo 다운로드가 완료되었습니다.
 echo 다운이 끝날시
